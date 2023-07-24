37c657aa4a9b00d35cf974ee00ad0c2aa74cc269
Sadly Harry <chanchoonchan@hotmail.sg>
2023-07-24 09:55:02 +0800

    Initial Commit

---
+++ CertStream.py
@@ -0,0 +1,117 @@
+"""This module starts CertStream, a background program.
+CertStream continuously monitors Certificate Transparency logs for newly
+registered domains. If the newly registered domain is a domain of intereest,
+the domain will be logged.
+"""
+import os
+import sys
+if getattr(sys, 'frozen', False):  # Support correct detection of input files
+    os.chdir(os.path.dirname(sys.executable))
+elif __file__:
+    os.chdir(os.path.dirname(__file__))
+
+import certstream
+import re
+
+from src.Commons import Database, FileHandler, Logger, Sanitiser, Utils
+
+
+class CertStream:
+
+    DEFAULT_CTSERVER: str = 'wss://certstream.calidog.io'
+    DEFAULT_INPUT: str = 'input/input_regex.txt'
+    DEFAULT_OUTPUT: str = 'logs/certstream.txt'
+
+    def __init__(self):
+        config: dict = Utils.load_config()
+
+        try:
+            self.ctserver: str = str(config['certstream']['server'])
+        except Exception:
+            Logger.warning('CertStream Server not found. '
+                           f'Defaulting to {CertStream.DEFAULT_CTSERVER}')
+            self.ctserver: str = CertStream.DEFAULT_CTSERVER
+
+        try:
+            self.input: str = str(config['certstream']['input'])
+        except Exception:
+            Logger.warning('Regex filename not found. '
+                           f'Defaulting to {CertStream.DEFAULT_INPUT}')
+            self.input: str = CertStream.DEFAULT_INPUT
+        FileHandler.check_availability(self.input)
+
+        try:
+            self.output: str = str(config['certstream']['output'])
+        except Exception:
+            Logger.warning('Output filename not found. '
+                           f'Defaulting to {CertStream.DEFAULT_OUTPUT}')
+            self.output: str = CertStream.DEFAULT_OUTPUT
+        FileHandler.check_availability(self.output)
+        FileHandler.clear(self.output)
+
+        regexes: list[str] = FileHandler.read(self.input)
+        self.pattern: re.Pattern = self.compile_regexes(regexes)
+        self.database: Database = Database(config)
+
+    def compile_regexes(self, regexes: list[str]) -> str:
+        """This method compiles regexes into a single regex.
+        """
+        # Test each regex.
+        if not regexes:
+            Logger.exception(f'No Regexes found in {self.input}')
+            raise SystemExit()
+        for r in regexes:
+            try:
+                re.compile(r)
+            except re.error:
+                Logger.exception(re.error(f"Invalid Regex: {r}"))
+                raise SystemExit()
+        return re.compile('(?:% s)' % '|'.join(regexes))
+
+    def is_heartbeat_msg(self, message: dict) -> bool:
+        """This method checks if the message is a heartbeat message.
+        """
+        return message['message_type'] == 'heartbeat'
+
+    def is_certupdate_msg(self, message: dict) -> bool:
+        """This method checks if the message is a certupdate message.
+        """
+        return message['message_type'] == 'certificate_update'
+
+    def is_relevant(self, domain: str) -> bool:
+        """This method checks if the domain is a domain of interest.
+        """
+        return self.pattern.match(domain)
+
+    def callback(self, message, context) -> None:
+        """This method is called when a new CT log is recieved.
+        """
+        if self.is_heartbeat_msg(message):
+            return
+        if self.is_certupdate_msg(message):
+            domains: list[str] = message['data']['leaf_cert']['all_domains']
+            san_domains: list[str] = Sanitiser.sanitise_domains(domains)
+            relv_domains: list[str] = [d for d in san_domains
+                                       if self.is_relevant(d)]
+            if not relv_domains:
+                return
+            for d in relv_domains:
+                Logger.info(f'New Domain: {d}')
+            self.database.insert(relv_domains)
+
+    def start(self):
+        """This method starts CertStream. Blocking code!
+        """
+        Logger.info('CertStream has started.')
+        # Blocking Code.
+        certstream.listen_for_events(self.callback, url=self.ctserver)
+        # Shutdown procedure
+        self.database.export()
+        Logger.info('CertStream has been shutdown.')
+
+
+if __name__ == "__main__":
+    """Main code to start certstream.
+    """
+    cs: certstream = CertStream()
+    cs.start()
---
+++ src/Commons.py
@@ -0,0 +1,304 @@
+"""This modules contains reusable attributes and methods.
+"""
+from functools import wraps
+import logging
+import os
+import pandas as pd
+from pathlib import Path
+import sqlite3
+from urllib.parse import urlparse
+import yaml
+
+
+class Database:
+    """SQLite Database Utility.
+    Simple SQL Database to store domain information
+    """
+
+    DEFAULT_DB_FILE: str = 'logs/CertStream.db'
+    DEFAULT_OUTPUT: str = 'logs/CertStream.csv'
+
+    def __init__(self, config: dict) -> None:
+        """Initialise Database with input configuration.
+        Initialise Cursor and Connection too.
+
+        Args:
+            config (dict): Configuration fields.
+        """
+        try:
+            self.database_filename: str = str(
+                config['database']['filename'])
+        except Exception:
+            Logger.warning('Database Filename not found. '
+                           f'Defaulting to {Database.DEFAULT_DB_FILE}')
+            self.database_filename: str = Database.DEFAULT_DB_FILE
+        FileHandler.check_availability(self.database_filename)
+
+        try:
+            self.output_filename: str = str(
+                config['certstream']['output'])
+        except Exception:
+            Logger.warning('Output Filename not found. '
+                           f'Defaulting to {Database.DEFAULT_OUTPUT}')
+            self.output_filename: str = Database.DEFAULT_OUTPUT
+        FileHandler.check_availability(self.output_filename)  # Early checking.
+
+        # Build DB
+        self.con: sqlite3.Connection = sqlite3.connect(self.database_filename)
+        self.cur: sqlite3.Cursor = self.con.cursor()
+
+        # Note SQLite database can accept NULL as primary keys :'(
+        self.cur.execute('DROP TABLE IF EXISTS certtransp_domains')
+        self.cur.execute("""CREATE TABLE certtransp_domains(
+                            domain TEXT NOT NULL PRIMARY KEY)""")
+
+    def insert(self, datalines: list[str]) -> None:
+        """Insert many new values into database.
+
+        Args:
+            datalines (list[str]): Data to be added.
+        """
+        stmt: str = """INSERT OR REPLACE INTO certtransp_domains(domain)
+                       VALUES(?)"""
+        datalines = [(d,) for d in datalines]
+        self.cur.executemany(stmt, datalines)
+        self.con.commit()
+
+    def execute(self, command: str) -> pd.DataFrame | None:
+        """Execute SQL Command.
+
+        Args:
+            command (str): SQL Command.
+
+        Returns:
+            pd.DataFrame | None: Results of SQL Command.
+        """
+        # TEST: Assert command is not empty.
+        assert (command)
+
+        try:
+            if command.lower().startswith('select'):
+                return pd.read_sql(command, self.con)
+            else:
+                self.cur.execute(command)
+                self.con.commit()
+        except sqlite3.ProgrammingError:
+            raise sqlite3.ProgrammingError(
+                'Executing more than 1 SQL statement. '
+                'Please check your command!')
+
+    def export(self) -> None:
+        """Export Database to CSV.
+        """
+        df: pd.DataFrame = self.execute(
+            'SELECT domain from certtransp_domains')
+        datalines: list[str] = df['domain'].tolist()
+        FileHandler.save(self.output_filename, datalines)
+
+
+class FileHandler:
+    """File Handling Utility.
+    """
+
+    @staticmethod
+    def check_availability(filename: str) -> None:
+        """Check if file is available for use.
+        We can check if the file is not in use.
+
+        Args:
+            filename (str): Name of file to check.
+        """
+        # TEST: Assert filename is not empty and checks are valid
+        assert (filename)
+
+        # Create file if doesn't exist
+        if not os.path.exists(filename):
+            file: Path = Path(filename)
+            file.parent.mkdir(parents=True, exist_ok=True)  # Make parent dirs
+            file.write_text("")
+
+        # Check 2: Check if file is closed.
+        try:
+            with open(filename, 'a'):
+                pass
+        except IOError:
+            Logger.exception(IOError(f"{filename} is not available. "
+                                     "Please close this file."))
+            raise SystemExit()
+
+    @staticmethod
+    def read(filename: str) -> list[str]:
+        """Read data from disk.
+
+        Args:
+            filename (str): Input filename.
+
+        Returns:
+            list[str]: Lines of Text from file.
+        """
+        # TEST: Assert filename is not empty.
+        assert (filename)
+
+        FileHandler.check_availability(filename)
+        with open(filename, 'r') as f:
+            datalines: list[str] = f.read().splitlines()
+            # Remove empty lines & whitespaces
+            return [dataline.strip() for dataline in datalines if dataline]
+
+    @staticmethod
+    def save(filename: str, datalines: list[str]) -> None:
+        """Write data to disk.
+
+        Args:
+            filename (str): Output filename.
+            datalines (list[str]): Lines of Data to write.
+        """
+        # TEST: Assert filename is not empty.
+        assert (filename and datalines)
+
+        FileHandler.check_availability(filename)
+        with open(filename, 'a+') as f:
+            f.write('\n'.join(datalines) + '\n')
+
+    @staticmethod
+    def clear(filename: str) -> None:
+        """Clear data from disk.
+        """
+        # TEST: Assert filename is not empty.
+        assert (filename)
+
+        FileHandler.check_availability(filename)
+        with open(filename, 'w'):
+            pass
+
+
+class Logger:
+    """Logging Utility.
+    """
+
+    DEFAULT_LOGFILE: str = 'logs/CertStream.log'
+
+    def init(filename: str = DEFAULT_LOGFILE):
+        """Initialise Logger.
+
+        Args:
+            filename (str): Name of log file.
+                Defaults to DEFAULT_LOGIFLE.
+        """
+        # TEST: Assert filename is not empty.
+        assert (filename)
+
+        FileHandler.check_availability(filename)  # Create /logs if needed.
+        logging.basicConfig(
+            filename=filename,
+            level=logging.INFO,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            filemode='w',
+            force=True)
+
+    init()
+    LOGGER: logging.Logger = logging.getLogger(__name__)
+
+    @staticmethod
+    def log(msg: str) -> callable:
+        """Decorator for logging. DO NOT EDIT WITHOUT UNDERSTANDING!
+        This decorator accepts a message argument! :)
+
+        Args:
+            msg (str): Message to be logged.
+
+        Returns:
+            callable: Function with logging.
+        """
+        # TEST: Assert log message is present.
+        assert (msg)
+
+        def decorator(f: callable) -> callable:
+            @wraps(f)  # Helps with documentation.
+            def wrapper(*args, **kwargs):
+                Logger.info(f"[{f.__qualname__}] {msg}")
+                return f(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def info(msg: str) -> None:
+        # TEST: Assert msg is not empty.
+        assert (msg)
+
+        Logger.LOGGER.info(msg)
+        print(msg)
+
+    @staticmethod
+    def warning(msg: str) -> None:
+        assert (msg)
+
+        Logger.LOGGER.warning(msg)
+        print(msg)
+
+    @staticmethod
+    def exception(e: Exception, msg: str = '') -> None:
+        # TEST: Assert e is not empty.
+        assert (e)
+
+        if not msg:
+            msg = str(e)
+        msg: str = f'{type(e).__name__}: {msg}'
+        Logger.LOGGER.exception(msg)
+        print(msg)
+
+    @log('Closing Logger.')
+    def __del__():
+        """Terminates Logging when our logger object is destroyed.
+        """
+        logging.shutdown()
+
+
+class Sanitiser:
+    """This class sanitises domains.
+    """
+    def sanitise_domains(domains: list[str]) -> list[str]:
+        """Removes http prefix and path suffix.
+        Mere Defensive Programming.
+
+        Args:
+            domains (list[str]): Input domains to sanitise.
+
+        Returns:
+            list[str]: Sanitised domains.
+        """
+        sanitised: set[str] = set()
+        for d in domains:
+            # Proper format for urlparse
+            d = f'http://{d}' if '//' not in d[:8] else d
+            d = urlparse(d).netloc  # Extract domain
+            d = d.removeprefix('www.').removeprefix('*.')
+            sanitised.add(d)
+        return list(sanitised)
+
+
+class Utils:
+    """Global Common Utilities.
+    """
+
+    DEFAULT_CONFIGFILE: str = 'input/config.yaml'
+
+    @staticmethod
+    def load_config(filename: str = DEFAULT_CONFIGFILE) -> dict:
+        """Load configuration field from configuration file.
+
+        Args:
+            filename (str, optional): Name of Configuration File.
+                Defaults to CONFIG_FILENAME.
+
+        Returns:
+            dict: Key-Value pairs of Configuration field.
+        """
+
+        # TEST: Assert filename is not empty.
+        assert (filename)
+
+        FileHandler.check_availability(filename)
+        with open(filename, 'r') as f:
+            return yaml.safe_load(f)
